6f7dd3b38688ec00cbfec13ac085ce48a37dcf30
Taopaipai300 <taofeeksanni1991@gmail.com>
2023-02-23 13:52:15 +0000

    This is fish

---
+++ src/main/scala/Inheritance/Rectangle.scala
@@ -0,0 +1,13 @@
+package Inheritance
+
+// Part 1
+/*
+// Here Polygon is a superclass and Rectangle is a subclass and Rectangle is inheriting from Polygon
+
+class Rectangle(var width: Double, var height: Double) extends Polygon {
+  override def area: Double = width * height;
+}
+*/
+
+
+// Part 2
